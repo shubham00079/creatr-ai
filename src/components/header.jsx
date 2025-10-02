2609cd43d2cddc20836be2a2f6f0cb67805f7a1f
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2025-10-02 12:14:51 +0530

    Link addition

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,49 @@
 'use client';
 import { useStoreUser } from '@/hooks/use-store-user';
-import {
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from '@clerk/nextjs';
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 import { Authenticated, Unauthenticated } from 'convex/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { BarLoader } from 'react-spinners';
 
 const Header = () => {
   const { isLoading, isAuthenticated } = useStoreUser();
-  console.log('isLoading', isLoading);
-  console.log('isAuthenticated', isAuthenticated);
+  const path = usePathname();
   return (
     <header className='fixed top-6 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-3xl px-4'>
       <div
         className='backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-4 
           sm:px-6 md:px-8 py-3 flex items-center justify-between gap-2'
       >
+        <Link href={isAuthenticated ? '/feed' : '/'} className='flex-shrink-0'>
+          <Image
+            src='/logo.png'
+            alt='Creatr Logo'
+            width={96}
+            height={32}
+            className='h-8 sm:h-10 w-auto object-contain'
+          />
+        </Link>
+
+        {path === '/' && (
+          <div className='hidden lg:flex space-x-6 flex-1 justify-center'>
+            <Link
+              href='#features'
+              className='text-white font-medium transition-all duration-300 hover:text-purple-300 curson-pointer'
+            >
+              Features
+            </Link>
+
+            <Link
+              href='#testimonials'
+              className='text-white font-medium transition-all duration-300 hover:text-purple-300 curson-pointer'
+            >
+              Testimonials
+            </Link>
+          </div>
+        )}
         <Unauthenticated>
           <SignInButton />
           <SignUpButton>
