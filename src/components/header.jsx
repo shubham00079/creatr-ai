510aa5fbac789d7daa0c13c48b6a317a1f3c7b24
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2025-10-05 11:22:00 +0530

    header update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,6 +13,11 @@ import { LayoutDashboard } from 'lucide-react';
 const Header = () => {
   const { isLoading, isAuthenticated } = useStoreUser();
   const path = usePathname();
+
+  if (path.includes('/dashboard')) {
+    return null;
+  }
+
   return (
     <header className='fixed top-6 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-3xl px-4'>
       <div
